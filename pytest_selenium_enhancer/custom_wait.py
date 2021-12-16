c8f014bd305dbfab41135658bece683e7d5d1ac5
mihairus <mihai.rus@ailylabs.com>
2021-12-16 12:29:11 +0200

    Increase timeout to 1 minute for non-static methods in custom_wait.py

--- pytest_selenium_enhancer/custom_wait.py
+++ pytest_selenium_enhancer/custom_wait.py
@@ -15,19 +15,19 @@ class CustomWait:
             'implicit' in driver.capabilities['timeouts'] and driver.capabilities['timeouts']['implicit'] != 0 \
             else 30
 
-    def wait_for_element_present(self, by=By.XPATH, value=None, text=None, timeout=10):
+    def wait_for_element_present(self, by=By.XPATH, value=None, text=None, timeout=60):
         if text is not None:
             value = value % text
         wait = WebDriverWait(self.driver, timeout)
         return wait.until(EC.presence_of_element_located((by, value)))
 
-    def wait_for_element_visible(self, by=By.XPATH, value=None, text=None, timeout=10):
+    def wait_for_element_visible(self, by=By.XPATH, value=None, text=None, timeout=60):
         if text is not None:
             value = value % text
         wait = WebDriverWait(self.driver, timeout)
         return wait.until(EC.visibility_of_element_located((by, value)))
 
-    def wait_for_element_not_visible(self, by=By.XPATH, value=None, text=None, timeout=5):
+    def wait_for_element_not_visible(self, by=By.XPATH, value=None, text=None, timeout=60):
         sleep(2)
         if text is not None:
             value = value % text
@@ -38,19 +38,19 @@ class CustomWait:
         self.driver.implicitly_wait(self.implicit_wait)
         return result
 
-    def wait_for_element_clickable(self, by=By.XPATH, value=None, text=None, timeout=10):
+    def wait_for_element_clickable(self, by=By.XPATH, value=None, text=None, timeout=60):
         if text is not None:
             value = value % text
         wait = WebDriverWait(self.driver, timeout)
         return wait.until(EC.element_to_be_clickable((by, value)))
 
-    def wait_for_child_element_visible(self, parent_element, by=By.XPATH, value=None, text=None, timeout=10):
+    def wait_for_child_element_visible(self, parent_element, by=By.XPATH, value=None, text=None, timeout=60):
         if text is not None:
             value = value % text
         wait = WebDriverWait(self.driver, timeout)
         return wait.until(CEC.visibility_of_child_element_located(parent_element, (by, value)))
 
-    def wait_for_child_element_not_visible(self, parent_element, by=By.XPATH, value=None, text=None, timeout=5):
+    def wait_for_child_element_not_visible(self, parent_element, by=By.XPATH, value=None, text=None, timeout=60):
         if text is not None:
             value = value % text
 
@@ -61,11 +61,11 @@ class CustomWait:
         self.driver.implicitly_wait(self.implicit_wait)
         return result
 
-    def wait_for_the_attribute_value(self, element, attribute, value, timeout=10):
+    def wait_for_the_attribute_value(self, element, attribute, value, timeout=60):
         wait = WebDriverWait(self.driver, timeout)
         return wait.until(CEC.wait_for_the_attribute_value(element, attribute, value))
 
-    def wait_for_the_attribute_contain_value(self, element, attribute, value, timeout=10):
+    def wait_for_the_attribute_contain_value(self, element, attribute, value, timeout=60):
         wait = WebDriverWait(self.driver, timeout)
         return wait.until(CEC.wait_for_the_attribute_contain_value(element, attribute, value))
 
